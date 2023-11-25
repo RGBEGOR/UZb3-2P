2b1efd132de9e20d2b86286f71116327eef0d06d
qwerty <qwerty@gmail.com>
2023-11-25 13:24:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,266 @@
+const headElem = document.getElementById("head");
+const buttonsElem = document.getElementById("buttons");
+const pagesElem = document.getElementById("pages");
+
+class Quiz
+{
+	constructor(type, questions, results)
+	{
+
+		this.type = type;
+
+		this.questions = questions;
+
+		this.results = results;
+
+		this.score = 0;
+
+		this.result = 0;
+
+		this.current = 0;
+	}
+
+	Click(index)
+	{
+		let value = this.questions[this.current].Click(index);
+		this.score += value;
+
+		let correct = -1;
+
+
+		if(value >= 1)
+		{
+			correct = index;
+		}
+		else
+		{
+
+			for(let i = 0; i < this.questions[this.current].answers.length; i++)
+			{
+				if(this.questions[this.current].answers[i].value >= 1)
+				{
+					correct = i;
+					break;
+				}
+			}
+		}
+
+		this.Next();
+
+		return correct;
+	}
+
+
+	Next()
+	{
+		this.current++;
+		
+		if(this.current >= this.questions.length) 
+		{
+			this.End();
+		}
+	}
+
+
+	End()
+	{
+		for(let i = 0; i < this.results.length; i++)
+		{
+			if(this.results[i].Check(this.score))
+			{
+				this.result = i;
+			}
+		}
+	}
+} 
+
+
+class Question 
+{
+	constructor(text, answers)
+	{
+		this.text = text; 
+		this.answers = answers; 
+	}
+
+	Click(index) 
+	{
+		return this.answers[index].value; 
+	}
+}
+
+
+class Answer 
+{
+	constructor(text, value) 
+	{
+		this.text = text; 
+		this.value = value; 
+	}
+}
+
+
+class Result 
+{
+	constructor(text, value)
+	{
+		this.text = text;
+		this.value = value;
+	}
+
+
+	Check(value)
+	{
+		if(this.value <= value)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
+}
+
+
+const results = 
+[
+	new Result("Ну это прям позор", 0),
+	new Result("Ну уже неплохо", 3),
+	new Result("Ваш уровень выше среднего", 4),
+	new Result("Поздравляем Вы успешно усвоили эту тему", 5)
+];
+
+const questions = 
+[
+	new Question(" Когда Арабский халифат завоевал государство Сасанидов (Иран). Захватив Иран, арабы направились на", 
+	[
+		new Answer('Китай ', 0),
+		new Answer("Среднею Азию ", 1),
+		new Answer("Испанию", 0),
+		new Answer("Иран", 0)
+	]),
+	new Question(" Столица Хорасана при арабах", 
+	[
+		new Answer('Мерв', 1),
+		new Answer("Вена", 0),
+		new Answer("Нерв", 0),
+		new Answer("Мараканда", 0)
+	]),
+	new Question("В период с 705 по 715 год - наместник Хорасана .... захватил территорию Турана Средней Азии  ", 
+	[
+		new Answer('Салахаддин', 0),
+		new Answer("Ат Таррик", 0),
+		new Answer("Абу Бакр", 0),
+		new Answer("Кутейба ибн Муслим", 1)
+	]),
+	new Question("С какого века арабы начли массовые набеги на Среднюю Азии (ТУРАН, МАВЕРАННАХР, ХОРАСАН)", 
+	[
+		new Answer('6 век ', 0),
+		new Answer("7 век", 1),
+		new Answer("8 век", 0),
+		new Answer("9 век", 0)
+	]),
+	new Question("Что исповедавали В Согде, Хорасане, Туране до нашествия арабов? ", 
+	[
+		new Answer('Зороастризм , Буддизм, Тенгризм ', 1),
+		new Answer("Ислам", 0),
+		new Answer("Христьянство", 0),
+		new Answer("нет ответа", 0)
+
+	]),
+];
+
+
+const quiz = new Quiz(1, questions, results);
+
+Update();
+
+
+function Update()
+{
+
+	if(quiz.current < quiz.questions.length) 
+	{
+
+		headElem.innerHTML = quiz.questions[quiz.current].text;
+
+
+		buttonsElem.innerHTML = "";
+
+
+		for(let i = 0; i < quiz.questions[quiz.current].answers.length; i++)
+		{
+			let btn = document.createElement("button");
+			btn.className = "button";
+
+			btn.innerHTML = quiz.questions[quiz.current].answers[i].text;
+
+			btn.setAttribute("index", i);
+
+			buttonsElem.appendChild(btn);
+		}
+		
+
+		pagesElem.innerHTML = (quiz.current + 1) + " / " + quiz.questions.length;
+
+		Init();
+	}
+	else
+	{
+
+		buttonsElem.innerHTML = "";
+		headElem.innerHTML = quiz.results[quiz.result].text;
+		pagesElem.innerHTML = "Ваши очки: " + quiz.score;
+	}
+}
+
+function Init()
+{
+
+	let btns = document.getElementsByClassName("button");
+
+	for(let i = 0; i < btns.length; i++)
+	{
+
+		btns[i].addEventListener("click", function (e) { Click(e.target.getAttribute("index")); });
+	}
+}
+
+function Click(index) 
+{
+
+	let correct = quiz.Click(index);
+
+
+	let btns = document.getElementsByClassName("button");
+
+
+	for(let i = 0; i < btns.length; i++)
+	{
+		btns[i].className = "button button_passive";
+	}
+
+	if(quiz.type == 1)
+	{
+		if(correct >= 0)
+		{
+			btns[correct].className = "button button_correct";
+		}
+
+		if(index != correct) 
+		{
+			btns[index].className = "button button_wrong";
+		} 
+	}
+	else
+	{
+
+		btns[index].className = "button button_correct";
+	}
+
+
+	setTimeout(Update, 1000);
+}
+
+
